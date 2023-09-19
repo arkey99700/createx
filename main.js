92de9e69ace0605cec0ea141ff203189f8561156
Vlad <arkey99700@gmail.com>
2023-09-19 18:18:05 +0300

    add animation in figures section

--- main.js
+++ main.js
@@ -1,6 +1,76 @@
 const goToTopElement = document.querySelector(".go-to-top");
 const goToTopText = goToTopElement.querySelector(".go-to-top__text");
 const goToTopButton = goToTopElement.querySelector(".go-to-top__button");
+const figuresSection = document.querySelector(".figures");
+
+function animateFigure(figure, data) {
+  const animationDuration = 2500;
+  const figureProgressBar = figure.querySelector(".figures__item-progress-bar");
+  const figureProgressNumber = figure.querySelector(".figures__item-value");
+  const figureCircumference = 2 * Math.PI * 74;
+  const maxLength = (data.value * figureCircumference) / data.maxValue;
+  figureProgressBar.setAttribute(
+    "stroke-dasharray",
+    `${figureCircumference}px`
+  );
+  figureProgressBar.setAttribute(
+    "stroke-dashoffset",
+    `${figureCircumference}px`
+  );
+
+  let startTime;
+
+  requestAnimationFrame(function animationTick(currentTime) {
+    if (!startTime) {
+      startTime = currentTime;
+    }
+
+    let progress = (currentTime - startTime) / animationDuration;
+
+    if (progress > 1) progress = 1;
+
+    progress = 1 - Math.pow(1 - progress, 4);
+
+    figureProgressNumber.textContent =
+      Math.ceil(progress * data.value) + (data.isPercent ? "%" : "");
+    figureProgressBar.setAttribute(
+      "stroke-dashoffset",
+      `${figureCircumference - progress * maxLength}px`
+    );
+
+    if (progress < 1) {
+      requestAnimationFrame(animationTick);
+    }
+  });
+}
+
+function animateFigures() {
+  const limits = [
+    { value: 98, maxValue: 98, isPercent: true },
+    { value: 20, maxValue: 27 },
+    { value: 9452, maxValue: 12000 },
+    { value: 100, maxValue: 100, isPercent: true },
+  ];
+  const figuresElements = document.querySelectorAll(".figures__item");
+
+  figuresElements.forEach((figure, index) => {
+    animateFigure(figure, limits[index]);
+  });
+}
+
+function figuresObserverCallback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        animateFigures();
+        observer.disconnect();
+      }, 200);
+    }
+  });
+}
+
+const figuresObserver = new IntersectionObserver(figuresObserverCallback);
+figuresObserver.observe(figuresSection);
 
 window.addEventListener("scroll", () => {
   const rect = document.body.getBoundingClientRect();
@@ -17,5 +87,3 @@ window.addEventListener("scroll", () => {
     goToTopElement.style.opacity = 0;
   }
 });
-
-goToTopButton.addEventListener("click", () => {});
